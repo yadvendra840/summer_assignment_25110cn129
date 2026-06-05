a37e7b75d1c614cd113914c2d3cc823cc5549907
Yadvevndra singh <67135888+yadvendra840@users.noreply.github.com>
2026-06-05 16:56:28 +0530

    Add files via upload

---
+++ Day5/code20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int n,i ,largest=0;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for (i=2;i<=n;i++)
+    {
+        while (n%i==0)
+        {
+            largest=i;
+            n=n/i;
+        }
+    }
+    printf("largest prime factor:%d",largest);
+
+    return 0;
+
+}
